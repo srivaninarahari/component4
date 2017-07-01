3cb3bd83a6cc3ca77ee76259a6c44ce208da32d3
srivaninarahari <srivaninarahari18@gmail.com>
2017-07-01 17:06:45 +0530

    sree

--- src/app/login1/login1.component.ts
+++ src/app/login1/login1.component.ts
@@ -5,7 +5,7 @@ import { ViewEncapsulation} from '@angular/core';
   selector: 'app-login1',
   templateUrl: './login1.component.html',
   styleUrls: ['./login1.component.css'],
-   encapsulation:ViewEncapsulation.Native// here with style output displayed
+   encapsulation:ViewEncapsulation.None      // here with style output displayed
 })
 export class Login1Component implements OnInit {
 
